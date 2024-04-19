ac5b349b0f9721b89e89ed13a667e1746c44b4a8
Khuong Phan <khuonghstvk@gmail.com>
2024-04-19 10:19:32 +0700

    connect DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 
+const uri = process.env.ATLAS_URI;
 app.listen(3000, () => {
   console.log("Server running in port 3000!");
 });
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log("MongoDB connection failed: ", error.message));
