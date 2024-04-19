f8f055d6f8a653b7a763f8eb365435216f8b2e4a
Khuong Phan <khuonghstvk@gmail.com>
2024-04-19 09:40:41 +0700

    create header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Projects from "./pages/Projects";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
