677925d81ee31da990333c47d81610a6d7ee9cf6
Khuong Phan <khuonghstvk@gmail.com>
2024-05-06 13:18:06 +0700

    add pre install

--- api/index.js
+++ api/index.js
@@ -3,15 +3,17 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRouter = require("./routes/userRoute");
 const authRouter = require("./routes/authRoute");
-const postRouter = require("./routes/postRoute")
-const commentRouter = require("./routes/commentRoute")
-const cookieParser = require("cookie-parser")
-const cors = require('cors')
+const postRouter = require("./routes/postRoute");
+const commentRouter = require("./routes/commentRoute");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
+
 dotenv.config();
-const app = express();  
-app.use(express.json())
-app.use(cors({credentials: true, origin: "http://localhost:5173"}))
-app.use(cookieParser())
+const app = express();
+app.use(express.json());
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cookieParser());
 app.listen(4000, () => {
   console.log("Server running in port 4000!");
 });
@@ -21,18 +23,26 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log("MongoDB connection failed: ", error.message));
 
-  app.use("/api/users", userRouter);
-  app.use("/api/auth", authRouter);
-  app.use("/api/posts", postRouter);
-  app.use("/api/comments", commentRouter);
+const ___dirname = path.resolve();
 
-  app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
+app.use("/api/comments", commentRouter);
 
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    })
-  })
+app.use(express.static(path.join(___dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(___dirname, "client", "dist", "index.html"));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
