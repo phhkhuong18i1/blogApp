6448f6593370fa1b45032b84a4dc33456cdf9c60
Khuong Phan <khuonghstvk@gmail.com>
2024-05-07 08:27:13 +0700

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +8,12 @@ export default defineConfig({
     host: true,
     strictPort: true,
     port: 8000,
+    },
+  build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+        },
+      },
     },
 })
